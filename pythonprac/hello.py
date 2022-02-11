22830514502f34a290813feaf5d43e623a63d3f5
LeeSanghyeop <bugwibug12@gmail.com>
2022-02-12 06:23:07 +0900

    순위 제목 평점 스크랩

--- pythonprac/hello.py
+++ pythonprac/hello.py
@@ -1,1 +1,23 @@
-print("hello sparta")
+import requests
+from bs4 import BeautifulSoup
+
+# .: 메소드 ['key']: 딕셔너리 벨류
+# url에 직접적으로 요청하는 것이 아니라 headers라는 조건을 걸어서 간접적으로 요청함
+headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+data = requests.get('https://movie.naver.com/movie/sdb/rank/rmovie.nhn?sel=pnt&date=20200303',headers=headers)
+
+soup = BeautifulSoup(data.text, 'html.parser')
+
+trs = soup.select('#old_content > table > tbody > tr')
+
+for tr in trs:
+  img_tag = tr.select_one('td.ac > img')
+  a_tag = tr.select_one('td.title > div > a')
+  point_class = tr.select_one('td.point')
+  if img_tag is not None and a_tag is not None and point_class:
+    rank = img_tag['alt']
+    title = a_tag.text
+    point = point_class.text
+    print(rank + " " + title + " " + point)
+
+
